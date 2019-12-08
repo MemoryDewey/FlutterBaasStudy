d58e38c42e0b998e32cc820d0a45c6416350c12c
MemoryDewey <memorydewey@vip.qq.com>
2019-12-08 19:03:20 +0800

    更新代码

---
+++ android/app/src/debug/gen/com/baasstudy/baas_study/Manifest.java
@@ -0,0 +1,7 @@
+/*___Generated_by_IDEA___*/
+
+package com.baasstudy.baas_study;
+
+/* This stub is only used by the IDE. It is NOT the Manifest class actually packed into the APK */
+public final class Manifest {
+}
--- lib/pages/course_list_page.dart
+++ lib/pages/course_list_page.dart
@@ -1,5 +1,13 @@
 import 'package:baas_study/widgets/search_bar.dart';
 import 'package:flutter/material.dart';
+import 'package:gzx_dropdown_menu/gzx_dropdown_menu.dart';
+
+class SortCondition {
+  String name;
+  bool isSelected;
+
+  SortCondition({this.name, this.isSelected});
+}
 
 class CourseListPage extends StatefulWidget {
   final bool hideLeft;
@@ -16,68 +24,109 @@ class CourseListPage extends StatefulWidget {
 }
 
 class _CourseListPageState extends State<CourseListPage> {
+  List<String> _dropdownHeaderItems = ['课程类型', '默认排序', '全部分类'];
+  List<SortCondition> _filterConditions = [];
+  List<SortCondition> _sortConditions = [];
+  SortCondition _selectFilterCondition;
+  SortCondition _selectSortCondition;
+  GZXDropdownMenuController _controller = GZXDropdownMenuController();
+  GlobalKey _stackKey = GlobalKey();
+
+  @override
+  void initState() {
+    super.initState();
+    /// 课程类型
+    _filterConditions.add(SortCondition(name: '课程类型', isSelected: true));
+    _filterConditions.add(SortCondition(name: '免费课程', isSelected: false));
+    _filterConditions.add(SortCondition(name: '付费课程', isSelected: false));
+    _filterConditions.add(SortCondition(name: '直播课程', isSelected: false));
+    _filterConditions.add(SortCondition(name: '录播课程', isSelected: false));
+
+    /// 课程排序
+    _sortConditions.add(SortCondition(name: '默认排序',isSelected: true));
+    _sortConditions.add(SortCondition(name: '好评最高',isSelected: false));
+    _sortConditions.add(SortCondition(name: '人气最高',isSelected: false));
+    _sortConditions.add(SortCondition(name: '价格最高',isSelected: false));
+    _sortConditions.add(SortCondition(name: '价格最低',isSelected: false));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: PreferredSize(
-          child: AppBar(
-            elevation: 0,
-          ),
-          preferredSize: Size.fromHeight(0),
-        ),
-        body: Column(
-          children: <Widget>[
-            Container(
-              decoration: BoxDecoration(
-                gradient: LinearGradient(
-                  colors: [Color(0x66000000), Colors.transparent],
-                  begin: Alignment.topCenter,
-                  end: Alignment.bottomCenter,
+      appBar: PreferredSize(
+        child: AppBar(elevation: 0),
+        preferredSize: Size.fromHeight(0),
+      ),
+      body: Stack(
+        key: _stackKey,
+        children: <Widget>[
+          Column(
+            children: <Widget>[
+              Container(
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    colors: [Color(0x66000000), Colors.transparent],
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter,
+                  ),
+                ),
+                child: SearchBar(
+                  hideLeft: widget.hideLeft,
+                  defaultText: widget.keyWord,
+                  hint: '搜索课程',
+                  leftButtonClick: () {
+                    Navigator.pop(context);
+                  },
+                  onChanged: _onTextChange,
                 ),
               ),
-              child: SearchBar(
-                hideLeft: widget.hideLeft,
-                defaultText: widget.keyWord,
-                hint: '搜索课程',
-                leftButtonClick: () {
-                  Navigator.pop(context);
-                },
-                onChanged: _onTextChange,
-              ),
-            ),
-            Divider(height: 0),
-            Container(
-              height: 50,
-              color: Theme.of(context).cardColor,
-              child: GridView.count(
-                crossAxisCount: 3,
-                children: <Widget>[
-                  _dropdownMenu(
-                    dropdownValue: '默认排序',
-                    dropdownItems: ['默认排序', '好评最高', '人气最高', '价格最高'],
-                    onChanged: (String value){
-                      print(value);
-                    },
-                  ),
-                  _dropdownMenu(
-                    dropdownValue: '全部课程',
-                    dropdownItems: ['全部课程', '好评最高', '人气最高', '价格最高'],
-                    onChanged: (String value){
-                      print(value);
-                    },
-                  ),
-                  _dropdownMenu(
-                    dropdownValue: '课程类型',
-                    dropdownItems: ['课程类型', '好评最高', '人气最高', '价格最高'],
-                    onChanged: (String value){
-                      print(value);
-                    },
-                  ),
+              Divider(height: 0, color: Colors.grey),
+              GZXDropDownHeader(
+                height: 45,
+                style: TextStyle(color: Color(0xff666666), fontSize: 16),
+                dropDownStyle: TextStyle(
+                  color: Colors.blue,
+                  fontSize: 16,
+                ),
+                items: [
+                  GZXDropDownHeaderItem(_dropdownHeaderItems[0]),
+                  GZXDropDownHeaderItem(_dropdownHeaderItems[1]),
+                  GZXDropDownHeaderItem(_dropdownHeaderItems[2]),
                 ],
+                controller: _controller,
+                stackKey: _stackKey,
               ),
-            ),
-          ],
-        ));
+            ],
+          ),
+          GZXDropDownMenu(
+            controller: _controller,
+            animationMilliseconds: 250,
+            menus: [
+              GZXDropdownMenuBuilder(
+                dropDownHeight: 40.0 * _filterConditions.length,
+                dropDownWidget:
+                    _buildConditionListWidget(_filterConditions, (value) {
+                  _selectFilterCondition = value;
+                  _dropdownHeaderItems[0] = _selectFilterCondition.name;
+                  _controller.hide();
+                  setState(() {});
+                }),
+              ),
+              GZXDropdownMenuBuilder(
+                dropDownHeight: 40.0 * _sortConditions.length,
+                dropDownWidget:
+                _buildConditionListWidget(_sortConditions, (value) {
+                  _selectSortCondition = value;
+                  _dropdownHeaderItems[1] = _selectSortCondition.name;
+                  _controller.hide();
+                  setState(() {});
+                }),
+              ),
+            ],
+          )
+        ],
+      ),
+    );
   }
 
   Widget _dropdownMenu({
@@ -99,6 +148,63 @@ class _CourseListPageState extends State<CourseListPage> {
         }).toList(),
         onChanged: onChanged,
       ),
+    );
+  }
+
+  Widget _buildConditionListWidget(
+      items, void itemOnTap(SortCondition sortCondition)) {
+    return ListView.separated(
+      shrinkWrap: true,
+      scrollDirection: Axis.vertical,
+      itemCount: items.length,
+      // item 的个数
+      separatorBuilder: (BuildContext context, int index) =>
+          Divider(height: 1.0),
+      // 添加分割线
+      itemBuilder: (BuildContext context, int index) {
+        SortCondition goodsSortCondition = items[index];
+        return GestureDetector(
+          onTap: () {
+            for (var value in items) {
+              value.isSelected = false;
+            }
+            goodsSortCondition.isSelected = true;
+
+            itemOnTap(goodsSortCondition);
+          },
+          child: Container(
+//            color: Colors.blue,
+            height: 40,
+            child: Row(
+              children: <Widget>[
+                SizedBox(
+                  width: 16,
+                ),
+                Expanded(
+                  child: Text(
+                    goodsSortCondition.name,
+                    style: TextStyle(
+                      color: goodsSortCondition.isSelected
+                          ? Theme.of(context).primaryColor
+                          : Colors.black,
+                    ),
+                  ),
+                ),
+                goodsSortCondition.isSelected
+                    ? Icon(
+                        Icons.check,
+                        color: Theme.of(context).primaryColor,
+                        size: 16,
+                      )
+                    : SizedBox(),
+                SizedBox(
+                  width: 16,
+                ),
+              ],
+            ),
+          ),
+        );
+      },
     );
   }
 
