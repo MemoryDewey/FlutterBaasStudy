a4f34844865abf5f5e29cbebac40e48a9e6394c9
MemoryDewey <memorydewey@vip.qq.com>
2019-12-09 17:25:13 +0800

    更改封装的Provider，更新builder(即将被移除)为create

--- lib/providers/provider_manage.dart
+++ lib/providers/provider_manage.dart
@@ -3,16 +3,14 @@ import 'package:provider/provider.dart';
 import 'dark_mode_provider.dart';
 import 'user_provider.dart';
 
-List<SingleChildCloneableWidget> providers = [
-  ...independentServices
-];
+List<SingleChildCloneableWidget> providers = [...independentServices];
 
 ///独立的Provider
 List<SingleChildCloneableWidget> independentServices = [
   ChangeNotifierProvider<DarkModeProvider>(
-    builder: (context) => DarkModeProvider(),
+    create: (context) => DarkModeProvider(),
   ),
   ChangeNotifierProvider<UserProvider>(
-    builder: (context) => UserProvider(),
+    create: (context) => UserProvider(),
   )
 ];
